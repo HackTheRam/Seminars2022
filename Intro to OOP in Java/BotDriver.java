e7b084d962e05136d47a68e88773d8245e337743
bwees <brandonwees@gmail.com>
2022-04-08 14:22:34 -0400

    updated java seminar materials

---
+++ Intro to OOP in Java/BotDriver.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+import kareltherobot.*;
+
+public class BotDriver implements Directions
+{
+	public static void main(String args[])  
+	{
+		makeWorld();
+	} 
+
+	public static void makeWorld()
+	{
+		World.reset(); 
+		World.readWorld("world.kwld"); 
+		World.setBeeperColor(Color.magenta);
+		World.setStreetColor(Color.blue);
+		World.setNeutroniumColor(Color.green.darker());
+		World.setDelay(30);  
+		World.setVisible(true);
+	}
+}
--- Intro to OOP in Java/Driver.java
+++ Intro to OOP in Java/Driver.java
@@ -1,21 +1,7 @@
-import java.awt.Color;
-import kareltherobot.*;
-
-public class Driver implements Directions
+public class Driver
 {
 	public static void main(String args[])  
 	{
-		makeWorld();
-	} 
 
-	public static void makeWorld()
-	{
-		World.reset(); 
-		World.readWorld("world.kwld"); 
-		World.setBeeperColor(Color.magenta);
-		World.setStreetColor(Color.blue);
-		World.setNeutroniumColor(Color.green.darker());
-		World.setDelay(30);  
-		World.setVisible(true);
 	}
 }
